0893f6d54304be3f31a67d452a65d588bcdae8fd
mocidade <43942882+mocidade@users.noreply.github.com>
2018-10-07 23:43:36 -0300

    Mocidade
    Teste

---
+++ Mocidade/Mocidade/TipoPagina/Master/Master.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mocidade.TipoPagina.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+
+
+		}
+        private void MudaPagina1(object sender, EventArgs args)
+        {
+            Detail = new Conteudo();
+        }
+
+    }
+}
